a25a258b47abec8d0b928742a1d6b727982b212a
1-Shreyash <108738197+1-Shreyash@users.noreply.github.com>
2024-08-30 22:45:49 +0530

    Integrating Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,96 +1,80 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss"
+
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        white: "#fff",
-        midnightblue: {
-          "100": "#113ca5",
-          "200": "#0c2e82",
-          "300": "#0b2b82",
-          "400": "#0e0359",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
-        slateblue: "#123eaa",
-        mediumslateblue: {
-          "100": "#9480ff",
-          "200": "#7b66ff",
-          "300": "#6f5cf9",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
-        cornflowerblue: {
-          "100": "#a39adf",
-          "200": "#8b81d0",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
-        lavender: {
-          "100": "#f1efff",
-          "200": "#efebff",
-          "300": "#e3ebff",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
         },
-        steelblue: "#6d82b4",
-        ghostwhite: {
-          "100": "#f7f5ff",
-          "200": "#f3f1ff",
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
         },
-        darkslateblue: "#3d589b",
-        gainsboro: {
-          "100": "#e8e8e8",
-          "200": "#e6e6e6",
-          "300": "#ddd",
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
-        thistle: {
-          "100": "#d8d1ff",
-          "200": "#d4cdff",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
-        lightsteelblue: {
-          "100": "#a1adcb",
-          "200": "#9eabcd",
-        },
-        whitesmoke: "#ededed",
-      },
-      spacing: {},
-      fontFamily: {
-        "bai-jamjuree": "'Bai Jamjuree'",
-        outfit: "Outfit",
       },
       borderRadius: {
-        "26xl": "45px",
-        "27xl": "46px",
-        "3xs": "10px",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
-    },
-    fontSize: {
-      base: "16px",
-      "5xl": "24px",
-      lgi: "19px",
-      lg: "18px",
-      xs: "12px",
-      "37xl": "56px",
-      "26xl": "45px",
-      "15xl": "34px",
-      "53xl": "72px",
-      "39xl": "58px",
-      "24xl": "43px",
-      inherit: "inherit",
-    },
-    screens: {
-      mq1300: {
-        raw: "screen and (max-width: 1300px)",
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
-      mq1125: {
-        raw: "screen and (max-width: 1125px)",
-      },
-      mq800: {
-        raw: "screen and (max-width: 800px)",
-      },
-      mq450: {
-        raw: "screen and (max-width: 450px)",
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  corePlugins: {
-    preflight: false,
-  },
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
