51bb4dac766a939fb7f0ca233e57f7ea74a37661
1-Shreyash <108738197+1-Shreyash@users.noreply.github.com>
2024-08-31 17:50:42 +0530

    replaced components with shadcn wherever required.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,65 +1,76 @@
-"use client"
-import Link from 'next/link';
-import React from 'react';
+"use client";
+import Link from "next/link";
+import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 
 const Login: React.FC = () => {
   return (
-    <div className='flex min-h-screen min-w-screen justify-center items-center'>
-    <div className="flex flex-col items-center justify-between border border-gray-300 min-h-[70vh] w-fit rounded-2xl py-12 px-24">
-      
-      <div className='w-full flex flex-col justify-center items-center'>
-      <h1 className="text-2xl font-semibold mb-6">Log in to JobHuntly</h1>
-      
-      <form className="w-full max-w-sm">
-        <div className="mb-4">
-          <input
-            type="email"
-            placeholder="Username or Email"
-            className="min-w-80 px-4 py-2 border text-sm rounded-lg focus:outline-none focus:ring-[1px] focus:ring-gray-300"
-            required
-          />
-        </div>
+    <div className="flex min-h-screen min-w-screen justify-center items-center">
+      <Card className="flex flex-col items-center justify-between min-h-[70vh] w-fit rounded-2xl py-12 px-8">
+        {" "}
+        {/* Adjusted padding */}
+        <CardContent className="w-full flex flex-col justify-center items-center">
+          <h1 className="text-2xl font-semibold mb-6">Log in to JobHuntly</h1>
 
-        <button
-          type="submit"
-          className="w-full py-2 bg-[--primary-color] text-white rounded-lg hover:bg-[#4e49ee]"
-        >
-          Continue
-        </button>
-      </form>
+          <form className="w-full max-w-sm">
+            <div className="mb-4">
+              <Input
+                type="email"
+                placeholder="Username or Email"
+                className="min-w-80 text-sm"
+                required
+              />
+            </div>
 
-      <div className="flex items-center justify-center my-6 w-full max-w-sm">
-        <hr className="flex-grow border-gray-300" />
-        <span className="mx-4 text-gray-500">or</span>
-        <hr className="flex-grow border-gray-300" />
-      </div>
+            <Button
+              type="submit"
+              className="w-full py-2 bg-[--primary-color] text-white rounded-lg hover:bg-[#4e49ee]"
+            >
+              Continue
+            </Button>
+          </form>
 
-      <button
-        onClick={() => alert('Sign in with Google')}
-        className="w-full max-w-sm py-2 flex flex-row items-center justify-center bg-[#4285f4] text-white rounded-lg"
-      >
-        <div className='bg-white p-1 mx-2 rounded-lg'>
-        <FcGoogle size="1.5em"/>
-        </div>
-        <p>Continue with Google</p>
-      </button>
-      </div>
-      <div className='w-full flex flex-col justify-center items-center'>
-      <div className="flex items-center justify-center my-6 w-full max-w-sm">
-        <hr className="flex-grow border-gray-300" />
-        <span className="mx-4 text-gray-500 text-sm">
-          Don't have an JobHuntly account?{' '}</span>
-        <hr className="flex-grow border-gray-300" />
-      </div>
-        <Link
-          href="/signup"
-          className="w-48 py-1 flex justify-center items-center bg-white border-2 border-[--primary-color] text-[--primary-color] rounded-lg hover:bg-gray-100"
-        >
+          <div className="flex items-center justify-center my-6 w-full max-w-sm">
+            <div className="flex items-center w-full">
+              <hr className="flex-grow border-gray-300" />
+              {/* Flex-grow ensures it fills available space */}
+              <span className="mx-4 text-gray-500">or</span>
+              <hr className="flex-grow border-gray-300" />{" "}
+              {/* Flex-grow ensures it fills available space */}
+            </div>
+          </div>
+
+          <Button
+            onClick={() => alert("Sign in with Google")}
+            className="w-full max-w-sm py-2 flex flex-row items-center justify-center bg-[#4285f4] text-white rounded-lg"
+          >
+            <div className="bg-white p-1 mx-2 rounded-lg">
+              <FcGoogle size="1.5em" />
+            </div>
+            <p>Continue with Google</p>
+          </Button>
+        </CardContent>
+        <CardContent className="w-full flex flex-col justify-center items-center">
+          <div className="flex items-center justify-center my-6 w-full max-w-sm">
+            <div className="flex items-center w-full">
+              <hr className="flex-grow border-gray-300" />
+              <span className="mx-4 text-gray-500 text-sm">
+                Don't have a JobHuntly account?{" "}
+              </span>
+              <hr className="flex-grow border-gray-300" />
+            </div>
+          </div>
+          <Link
+            href="/signup"
+            className="w-48 py-1 flex justify-center items-center bg-white border-2 border-[--primary-color] text-[--primary-color] rounded-lg hover:bg-gray-100"
+          >
             Sign Up
-        </Link>
-      </div>
-    </div>
+          </Link>
+        </CardContent>
+      </Card>
     </div>
   );
 };
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,6 +4,9 @@ import Link from 'next/link';
 import { ImOffice } from "react-icons/im";
 import { FaUser } from "react-icons/fa";
 import { useRouter } from 'next/navigation';
+import { Button } from "@/components/ui/button";
+import { Card} from "@/components/ui/card";
+
 
 const JoinPage = () => {
   const [isUser, setIsUser] = useState(false);
@@ -59,17 +62,17 @@ const JoinPage = () => {
       <h1 className="text-3xl font-medium mb-6">Join as a User or Company</h1>
 
       <div className="flex space-x-4 mb-6">
-        <div
+        <Card
           onClick={handleUserCheckboxChange}
           onMouseDown={handleUserMouseDown}
           onMouseUp={handleUserMouseUp}
           onMouseLeave={handleUserMouseUp}
-          className={`p-8 w-64 rounded-lg flex flex-col transition-transform duration-300 ${
+          className={`p-8 w-64 rounded-lg flex flex-col transition-transform duration-300 cursor-pointer ${
             isUser ? 'border-2 border-[--primary-color] bg-[#00000005]' : 'border-2 border-gray-300 bg-white text-gray-800'
           } ${isUserButtonPressed ? 'scale-95' : ''} hover:border-2 hover:border-[--primary-color] hover:bg-[#00000005] relative`}
         >
           <div>
-            <FaUser size="1.5em"/>
+            <FaUser size="1.5em" />
           </div>
           <div className='text-xl font-semibold mt-8 text-start'>
             I'm a user, looking for work
@@ -77,19 +80,19 @@ const JoinPage = () => {
           <div className={`w-6 h-6 border border-gray-300 rounded-full absolute top-3 right-3 flex justify-center items-center ${isUser ? 'bg-[--primary-color] border-none' : 'bg-white'}`}>
             <div className='w-3 h-3 border border-white rounded-full'></div>
           </div>
-        </div>
+        </Card>
 
-        <div
+        <Card
           onClick={handleCompanyCheckboxChange}
           onMouseDown={handleCompanyMouseDown}
           onMouseUp={handleCompanyMouseUp}
           onMouseLeave={handleCompanyMouseUp}
-          className={`p-8 w-64 rounded-lg flex flex-col transition-transform duration-300 ${
+          className={`p-8 w-64 rounded-lg flex flex-col transition-transform duration-300 cursor-pointer ${
             isCompanyManager ? 'border-2 border-[--primary-color] bg-[#00000005]' : 'border-2 border-gray-300 bg-white text-gray-800'
           } ${isCompanyButtonPressed ? 'scale-95' : ''} hover:border-2 hover:border-[--primary-color] hover:bg-[#00000005] relative`}
         >
           <div>
-            <ImOffice size="1.5em"/>
+            <ImOffice size="1.5em" />
           </div>
           <div className='text-xl font-semibold mt-8 text-start'>
             I am a Company Manager, hiring
@@ -97,10 +100,10 @@ const JoinPage = () => {
           <div className={`w-6 h-6 border border-gray-300 rounded-full absolute top-3 right-3 flex justify-center items-center ${isCompanyManager ? 'bg-[--primary-color] border-none' : 'bg-white'}`}>
             <div className='w-3 h-3 border border-white rounded-full'></div>
           </div>
-        </div>
+        </Card>
       </div>
 
-      <button
+      <Button
         disabled={isButtonDisabled}
         className={`px-6 py-2 font-semibold rounded-lg ${
           isButtonDisabled
@@ -110,11 +113,10 @@ const JoinPage = () => {
         onClick={handleCreateAccount}
       >
         {getButtonText()}
-      </button>
+      </Button>
 
       <div className="mt-6">
         <p>Already have an account?{' '}
-        
           <Link href="/login/" className="text-[--primary-color] hover:underline">
             Log in
           </Link>
