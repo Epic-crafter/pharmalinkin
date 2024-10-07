f923be41ff062f38697b45d3818b9fd415cf96a4
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2024-10-07 18:22:05 +0530

    first commit

--- src/app/(new)/[userId]/company/layout.tsx
+++ src/app/(new)/[userId]/company/layout.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import Dashboard from './dashboard/page'
-const page = () => {
-  return (
-    <div className="flex gap-5 max-md:flex-col">
-      <Dashboard/>
-    </div>
-  )
+import React, { ReactNode } from 'react';
+import Dashboard from './dashboard/page';
+
+interface Page {
+  children: ReactNode;
 }
 
-export default page
+const Page: React.FC<Page> = ({ children }) => {
+  return (
+    <div className="flex gap-5 max-md:flex-col">
+      <Dashboard />
+      {children}
+    </div>
+  );
+};
+
+export default Page;
