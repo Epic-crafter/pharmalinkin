fae361387e565d00eda9d668602f860dfae64e3c
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2024-10-17 22:36:56 +0530

    d

--- src/app/verified/[userId]/company/page.tsx
+++ src/app/verified/[userId]/company/page.tsx
@@ -5,12 +5,14 @@ import {Button} from '../../../../components/ui/button'
 import Dashboard from './dashboard/page';
 import Job from './job-posted/page'
 import Shortlist from './job-posted/shortlisted/page'
+import SignUp from './sign-up/index'
 const page = () => {
   return (
     <div className="min-h-screen bg-gray-100">
       {/* <Dashboard/> */}
       {/* <Job/> */}
-      <Shortlist/>
+      {/* <Shortlist/> */}
+      <SignUp/>
     </div>
   )
 }
---
+++ src/app/verified/[userId]/company/sign-up/components/PassInput.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { Input } from "../../../../../../components/ui/input";
+
+export const PasswordInput = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className="mb-4">
+      <Input
+        type={showPassword ? "text" : "password"}
+        placeholder="Password (6+ characters)"
+        className="w-full p-2 border rounded-md"
+      />
+      <div
+        className="text-sm mt-1 text-blue-600 hover:underline cursor-pointer"
+        onClick={() => setShowPassword(!showPassword)}
+      >
+        {showPassword ? "Hide" : "Show"}
+      </div>
+    </div>
+  );
+};
