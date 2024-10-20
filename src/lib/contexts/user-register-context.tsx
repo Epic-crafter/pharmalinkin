a47144029441cfe44fed13a7d212b51b01f161d3
meadarsh <itsadarsh33@gmail.com>
2024-10-21 00:53:03 +0530

    update

---
+++ src/lib/contexts/user-register-context.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useContext, useState } from 'react';
+
+// Create the context
+const RegistrationContext = createContext();
+
+// Create a provider component
+export const RegistrationProvider = ({ children }:any) => {
+  const [registrationData, setRegistrationData] = useState({
+    email: '',
+    phone: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    location: '',
+    mostRecentJobTitle: '',
+    collegeName: '',
+    collegeStartDate: '',
+    collegeEndDate: '',
+    degree: '',
+    fieldOfStudy: '',
+    preferredJobTitle: '',
+    preferredLocation: '',
+    openForRemote: false,
+  });
+
+  const updateRegistrationData = (field:any, value:any) => {
+    setRegistrationData((prevData) => ({
+      ...prevData,
+      [field]: value,
+    }));
+  };
+
+  return (
+    <RegistrationContext.Provider value={{ registrationData, updateRegistrationData }}>
+      {children}
+    </RegistrationContext.Provider>
+  );
+};
+
+export const useRegistrationContext = () => useContext(RegistrationContext);
