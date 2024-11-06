2d7278c0efb2dba10b04d2ed0296ce815f76c0ca
meadarsh <itsadarsh33@gmail.com>
2024-11-06 15:07:04 +0530

    Fix

--- src/app/verified/[userId]/Employee/jobs/[jobId]/page.tsx
+++ src/app/verified/[userId]/Employee/jobs/[jobId]/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { Badge } from "@/components/ui/badge";
-import { Dialog } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
@@ -19,19 +20,19 @@ import {
   FaRegHourglass,
   FaRegMoneyBillAlt,
   FaRegUser,
-  FaExternalLinkAlt, 
+  FaExternalLinkAlt,
   FaFacebook,
   FaFacebookSquare,
   FaLinkedin,
   FaTwitterSquare,
-  FaRupeeSign
+  FaRupeeSign,
 } from "react-icons/fa";
 
-const page = ({params}: any) => {
+const page = ({ params }: any) => {
   const router = useRouter();
   console.log(params);
-  const { jobId } = params; 
-  const {userId} = params;
+  const { jobId } = params;
+  const { userId } = params;
   console.log("JOB ID-----", jobId);
   const [job, setJob] = useState<any>();
   const [isDialogOpen, setIsDialogOpen] = useState(false);
@@ -46,9 +47,7 @@ const page = ({params}: any) => {
     fetchJobDetails();
   }, [jobId]);
 
-  const applyJob = () => {
-    setIsDialogOpen(true); // Open the dialog on button click
-  };
+
 
   const handleDialogConfirm = () => {
     setIsDialogOpen(false); // Close the dialog before redirecting
@@ -70,8 +69,9 @@ const page = ({params}: any) => {
             <div className="flex gap-2 items-center">
               <p className="text-md font-bold ">{job?.company?.companyName}</p>
               <div className="text-sm text-blue-400 cursor-pointer">
-
-              <a href={job?.company.website}><FaExternalLinkAlt  /></a>
+                <a href={job?.company.website}>
+                  <FaExternalLinkAlt />
+                </a>
               </div>
               {job?.status && (
                 <Badge className="text-xs font-medium text-blue-400 border-blue-400 bg-transparent">
@@ -79,10 +79,10 @@ const page = ({params}: any) => {
                 </Badge>
               )}
             </div>
-            
-
           </div>
-          <div><img src={job?.company.logo} alt="logo"/></div>
+          <div>
+            <img src={job?.company.logo} alt="logo" />
+          </div>
         </div>
 
         {/* Modal Body */}
@@ -110,16 +110,26 @@ const page = ({params}: any) => {
 
             {/* Stipend, Deadline */}
             <div className="flex flex-wrap gap-12 text-gray-500">
-              
               <div className="flex flex-col  gap-1 text-sm">
-                <span className="flex gap-1 items-center"><FaRegMoneyBillAlt className="text-gray-400" /> Salary</span>
-                
-                <span className="text-gray-800 flex">{job?.salaryCurrency==="INR" && <FaRupeeSign className="text-gray-600" />} {job?.salary}</span>
+                <span className="flex gap-1 items-center">
+                  <FaRegMoneyBillAlt className="text-gray-400" /> Salary
+                </span>
+
+                <span className="text-gray-800 flex">
+                  {job?.salaryCurrency === "INR" && (
+                    <FaRupeeSign className="text-gray-600" />
+                  )}{" "}
+                  {job?.salary}
+                </span>
               </div>
               <div className="flex flex-col  gap-1 text-sm">
-                <span className="flex gap-1 items-center"><FaRegHourglass className="text-gray-400" /> Apply by</span>
-                
-                <span className="text-gray-800">{job?.applicationDeadline}</span>
+                <span className="flex gap-1 items-center">
+                  <FaRegHourglass className="text-gray-400" /> Apply by
+                </span>
+
+                <span className="text-gray-800">
+                  {job?.applicationDeadline}
+                </span>
               </div>
             </div>
 
@@ -129,14 +139,14 @@ const page = ({params}: any) => {
                 <FaRegClock className="text-gray-400" />
                 <span>Posted {job?.postedDate}</span>
               </div>
-             
-              {job?.applicants &&
-              <div className="flex items-center gap-2">
-                {/* <FaCircle className="text-gray-400 w-2 h-2" /> */}
-                <FaRegUser className="text-gray-400" />
-                <span>{job?.applicants.length} applicants</span>
-              </div>
-             } 
+
+              {job?.applicants && (
+                <div className="flex items-center gap-2">
+                  {/* <FaCircle className="text-gray-400 w-2 h-2" /> */}
+                  <FaRegUser className="text-gray-400" />
+                  <span>{job?.applicants.length} applicants</span>
+                </div>
+              )}
             </div>
           </div>
 
@@ -147,81 +157,71 @@ const page = ({params}: any) => {
             <h3 className="text-lg font-semibold text-gray-700 mb-2">
               Description
             </h3>
-            <p className="text-gray-600 mb-3">
-              {job?.description}
-            </p>
+            <p className="text-gray-600 mb-3">{job?.description}</p>
           </div>
 
           {/* Skills Required */}
-          {job?.skillsRequired.length > 0 &&
-          <div>
-            <h3 className="text-lg font-semibold text-gray-700 mb-2">
-              Skill(s) Required
-            </h3>
-            <div className="flex flex-wrap gap-3">
-              {job?.skillsRequired?.map((skill: any, index: any) => (
-                <span
-                  key={index}
-                  className="bg-gray-100 text-gray-700 px-3 py-1 rounded-full text-sm"
-                >
-                  {skill}
-                </span>
-              ))}
+          {job?.skillsRequired.length > 0 && (
+            <div>
+              <h3 className="text-lg font-semibold text-gray-700 mb-2">
+                Skill(s) Required
+              </h3>
+              <div className="flex flex-wrap gap-3">
+                {job?.skillsRequired?.map((skill: any, index: any) => (
+                  <span
+                    key={index}
+                    className="bg-gray-100 text-gray-700 px-3 py-1 rounded-full text-sm"
+                  >
+                    {skill}
+                  </span>
+                ))}
+              </div>
             </div>
-          </div>
- }
+          )}
 
           {/* Requirements */}
           <div className="">
             <h3 className="text-lg font-semibold text-gray-700 mb-2">
               Requirements
-            </h3> 
-            <p className=" text-gray-600 py-3">
-              {job?.requirements}
-            </p>
-          </div> 
-{/* Social links */}
+            </h3>
+            <p className=" text-gray-600 py-3">{job?.requirements}</p>
+          </div>
+          {/* Social links */}
           <div className="flex text-gray-500 text-xl gap-4">
-            
-         <a href={job?.company.socialLinks.facebook}><FaFacebookSquare/> </a>
-         <a href={job?.company.socialLinks.linkedin}><FaLinkedin/></a>
-         <a href={job?.company.socialLinks.twitter}><FaTwitterSquare/></a>
-
+            <a href={job?.company.socialLinks.facebook}>
+              <FaFacebookSquare />{" "}
+            </a>
+            <a href={job?.company.socialLinks.linkedin}>
+              <FaLinkedin />
+            </a>
+            <a href={job?.company.socialLinks.twitter}>
+              <FaTwitterSquare />
+            </a>
           </div>
 
           <hr className="border-gray-300" />
-
-          {/* Apply Button */}
           <div className="flex justify-left mt-4">
-            <button onClick={()=>setIsDialogOpen(true)} className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 focus:outline-none">
-              Apply Now
-            </button>
+            <Dialog open={isDialogOpen} onOpenChange={()=>setIsDialogOpen(!isDialogOpen)}>
+              <DialogTrigger asChild>
+                <Button variant="outline">Apply now</Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogTitle className="text-lg font-semibold mb-2">
+                  Confirm Application
+                </DialogTitle>
+                <p className="text-gray-600 mb-4">
+                  Are you sure you want to apply for this job?
+                </p>
+                <div className="flex justify-end space-x-3">
+                  <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+                    Yes, Apply
+                  </button>
+                </div>
+              </DialogContent>
+            </Dialog>
           </div>
         </div>
       </div>
-      {isDialogOpen && 
-      
-      <Dialog open={isDialogOpen} onOpenChange={()=>setIsDialogOpen(!isDialogOpen)}>
-        <div className="">
-          <h3 className="text-lg font-semibold mb-2">Confirm Application</h3>
-          <p className="text-gray-600 mb-4">Are you sure you want to apply for this job?</p>
-          <div className="flex justify-end space-x-3">
-            <button
-              onClick={handleDialogCancel}
-              className="px-4 py-2 bg-gray-200 rounded-lg hover:bg-gray-300"
-            >
-              Cancel
-            </button>
-            <button
-              onClick={handleDialogConfirm}
-              className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-            >
-              Yes, Apply
-            </button>
-          </div>
-        </div>
-      </Dialog>
-}
     </div>
   );
 };
