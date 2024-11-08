79aef04e60f08ffbc8b17e6f7c8e2d72f9004f7a
meadarsh <itsadarsh33@gmail.com>
2024-11-08 17:07:39 +0530

    Added context

--- src/app/verified/[userId]/Employer/job-posted/[jobId]/layout.tsx
+++ src/app/verified/[userId]/Employer/job-posted/[jobId]/layout.tsx
@@ -3,7 +3,7 @@ import Sidebar from "./components/Sidebar";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 
-const layout = ({ children }: any) => {
+const layout = ({ children,jobId }: any) => {
   return (
     <div className="h-[calc(100vh-80px)] flex">
       <Sidebar />
---
+++ src/lib/contexts/jobId-context.tsx
@@ -0,0 +1,31 @@
+// context/JobContext.tsx
+import React, { createContext, useContext, ReactNode } from "react";
+
+type JobContextType = {
+  jobId: string | null;
+  setJobId: (id: string) => void;
+};
+
+const JobContext = createContext<JobContextType | undefined>(undefined);
+
+export const useJobContext = () => {
+  const context = useContext(JobContext);
+  if (!context) {
+    throw new Error("useJobContext must be used within a JobProvider");
+  }
+  return context;
+};
+
+type JobProviderProps = {
+  children: ReactNode;
+};
+
+export const JobProvider = ({ children }: JobProviderProps) => {
+  const [jobId, setJobId] = React.useState<string | null>(null);
+
+  return (
+    <JobContext.Provider value={{ jobId, setJobId }}>
+      {children}
+    </JobContext.Provider>
+  );
+};
