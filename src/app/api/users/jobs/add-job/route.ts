03d3f867266100dcf98e93ebcc078c12b74c84dc
Aryan Chaurasiya <aryanchaurasia1806@gmail.com>
2025-01-19 14:56:17 +0530

    Added some field on job-posts

--- src/app/api/users/jobs/add-job/route.ts
+++ src/app/api/users/jobs/add-job/route.ts
@@ -3,6 +3,9 @@ import { connectToDatabase } from "@/lib/mongo";
 import { Job } from "@/models/job-post";
 import { CompanyProfile } from "@/models/company-profile"; // Import company profile model
 import { getToken } from "next-auth/jwt";
+import path from "path";
+import DatauriParser from "datauri/parser";
+import {cloudinary} from "@/lib/actions/cloudinary";
 
 // POST request handler for creating a new job post
 export async function POST(req: NextRequest) {
@@ -33,22 +36,148 @@ export async function POST(req: NextRequest) {
     }
 
     // Parse the request body
-    const {
+    // const {
+    //   title,
+    //   description,
+    //   requirements,
+    //   location,
+    //   country,
+    //   jobType,
+    //   jobcategory,
+    //   salary,
+    //   salaryCurrency,
+    //   experienceLevel,
+    //   applicationDeadline,
+    //   category,
+    //   skillsRequired,
+    //   image,
+    // } = await req.json();
+    
+    //  console.log("--------->",image);
+    
+    const formData = await req.formData(); 
+    const data:any = {};
+    for(let [key,value] of formData.entries()){
+      data[key] = value;
+    }
+    // Destructring the data from formData
+    const { 
       title,
       description,
       requirements,
       location,
       country,
       jobType,
+      jobcategory,
       salary,
       salaryCurrency,
+      companyName,
+      companyWebsite,
       experienceLevel,
+      address,
+      contactperson,
+      mobile,
+      email,
+      postalcode,
+      salaryoffer,
+      qualification,
+      experience,
+      status,
+      skillsRequired,
       applicationDeadline,
       category,
-      skillsRequired,
-    } = await req.json();
+      coverimage,
+      video,
+      image,
+      file,
+      closingdate,
+      postedDate
+      } = data;
+    
+      // Creating uploading function on cloudinary
+    if(!image || !video){
+        return NextResponse.json({error:"Image and video is required",data:null},{status:400});
+    }
+    const parser = new DatauriParser();
 
-    // Validate if all required fields are present before creating the Job document
+    // 1. Upload images on cloudinary.
+    const Imagebuffer = Buffer.from(await image.arrayBuffer()); // Covert ArrayBuffer to ImageBuffer
+
+    // Upload the new image
+    const base64Image = parser.format(
+      path.extname(image.name).toString(),
+      Imagebuffer
+    );
+
+    if(!base64Image.content){
+      return NextResponse.json({error:null,message:"Failed to parse"})
+    }
+
+    const createdImage =  await cloudinary.uploader.upload(
+      base64Image.content,
+      {
+        resource_type:"image",
+      }
+    )
+
+    // 2. Upload the video on cloudinary
+    const Videobuffer = Buffer.from(await video.arrayBuffer());
+
+    const base64Video = parser.format(
+      path.extname(video.name).toString(),
+      Videobuffer
+    );
+
+    if(!base64Video.content){
+      return NextResponse.json({error:null,message:"Failed to parse"})
+    }
+
+    const createdVideo =  await cloudinary.uploader.upload(
+      base64Video.content,
+      {
+        resource_type:"video",
+      }
+    )
+
+    // 3. Upload files on cloudinary
+    const filebuffer = Buffer.from(await file.arrayBuffer());
+
+    const base64file = parser.format(
+      path.extname(file.name).toString(),
+      filebuffer
+    );
+
+    if(!base64file.content){
+      return NextResponse.json({error:null,message:"Failed to parse"})
+    }
+
+    const createdfile =  await cloudinary.uploader.upload(
+      base64Video.content,
+      {
+        resource_type:"auto",
+      }
+    )
+
+    //  4. Upload CoverImages on cloudinary
+     const coverimagebuffer = Buffer.from(await coverimage.arrayBuffer());
+
+    const base64coverImage = parser.format(
+      path.extname(coverimage.name).toString(),
+      coverimagebuffer
+    );
+
+    if(!base64coverImage.content){
+      return NextResponse.json({error:null,message:"Failed to parse"})
+    }
+
+    const createdcoverImage =  await cloudinary.uploader.upload(
+      base64coverImage.content,
+      {
+        resource_type:"image",
+      }
+    )
+
+  //   // Validate if all required fields are present before creating the Job document
     if (
       !title ||
       !description ||
@@ -70,6 +199,7 @@ export async function POST(req: NextRequest) {
       requirements,
       location,
       country,
+      jobcategory,
       jobType,
       salary,
       salaryCurrency,
@@ -78,9 +208,24 @@ export async function POST(req: NextRequest) {
       experienceLevel,
       applicationDeadline,
       category,
+      address,
+      contactperson,
+      mobile,
+      email,
+      postalcode,
+      salaryoffer,
+      qualification,
+      experience,
+      status,
       skillsRequired,
+      closingdate,
+      postedDate,
       createdBy: token.id,
-      company:companyProfile.id
+      company:companyProfile.id,
+      coverimageurl:createdcoverImage.secure_url, // set coveimagerurl from the cloudinary
+      videourl:createdVideo.secure_url,  // set videourl from the cloudinary
+      imageurl:createdImage.secure_url, // set galleryimageurl from the cloudinary
+      fileurl:createdfile.secure_url, // set fileurl from the cloudinary
     });
 
     // Save the job to the database
